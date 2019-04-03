e8869a382c5e9d03611f13dd6cfea55ad4140edb
Matthew Inman Cochrane <26103520+imatt611@users.noreply.github.com>
2019-04-03 11:57:09 -0700

    Simplify implementation (remove custom hook)

--- src/fooList.js
+++ src/fooList.js
@@ -1,10 +1,28 @@
-import React, { useRef, useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 import io from "socket.io-client";
 
-function useSocket(channel, onMessage) {
-	// We only want to emit "join-channel" once, so hold a ref to socket
-	const socketRef = useRef(io("http://localhost:3001"));
-	socketRef.current.emit("join-channel", channel);
+export default function FooList(props) {
+	const [names, setNames] = useState([]);
+
+	const handleMessage = msg => {
+		if (msg.url) {
+			fetch(msg.url).then(res =>
+				setNames(prevState => [
+					...prevState,
+					`${res.first_name} ${res.last_name}`
+				])
+			);
+		} else {
+			setNames(prevState => [
+				...prevState,
+				`${msg.first_name} ${msg.last_name}`
+			]);
+		}
+	};
+
+	// We only want to emit "join-channel" once
+	const socket = io();
+	socket.emit("join-channel", `id-${props.barId}`);
 
 	// We only want to create a listener once, but want it off if the component
 	//  is destroyed
@@ -12,27 +30,14 @@ function useSocket(channel, onMessage) {
 		/***
 		 * When running test, this code is never reached.
 		 **/
-		const socket = socketRef.current;
-		socket.on("message", onMessage);
-		return () => socket.off("message", onMessage);
+		debugger;
+		socket.on("message", handleMessage);
+		return () => socket.off("message", handleMessage);
 	});
-}
 
-export default function FooList(props) {
-	const [name, setName] = useState("");
-
-	const handleMessage = msg => {
-		// In reality, we sometimes needs to fetch and sometimes don't
-		fetch(msg.url).then(res =>
-			setName(`${res.first_name} ${res.last_name}`)
-		);
-	};
-
-	useSocket(`id-${props.barId}`, handleMessage);
-
-	return name && name.map((fooProps, i) => <Foo key={i} {...fooProps} />);
+	return names && names.map((name, i) => <Foo key={i} name={name} />);
 }
 
 function Foo(props) {
-	return <div>{JSON.stringify(props)}</div>;
+	return <div>{props.name}</div>;
 }
